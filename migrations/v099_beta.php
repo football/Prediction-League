7d8b92f1dfe6df95b64a212cdbb3c2d04091881b
football <phpbb.football@gmx.de>
2017-09-09 13:51:15 +0200

    Version 0.9.9

--- migrations/v099_beta.php
+++ migrations/v099_beta.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package Football Football v0.9.9b1
+* @package Football Football v0.9.9
 * @copyright (c) 2017 football (http://football.bplaced.net)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
@@ -9,11 +9,11 @@
 
 namespace football\football\migrations;
 
-class v099_beta1 extends \phpbb\db\migration\migration
+class v099_beta extends \phpbb\db\migration\migration
 {
 	public function effectively_installed()
 	{
-		return isset($this->config['football_version']) && version_compare($this->config['football_version'], '0.9.9b1', '>=');
+		return isset($this->config['football_version']) && version_compare($this->config['football_version'], '0.9.9', '>=');
 	}
 	
 	static public function depends_on()
@@ -28,7 +28,7 @@ class v099_beta1 extends \phpbb\db\migration\migration
 			array('config.add', array('football_time_shift', '1', '0')),
 			array('config.add', array('football_display_last_users', '5', '0')),
 			array('config.add', array('football_display_last_results', '0', '0')),
-			array('config.update', array('football_version', '0.9.9b1', '0')),
+			array('config.update', array('football_version', '0.9.9', '0')),
 		);
 	}
 }
